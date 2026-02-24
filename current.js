6fe849a39db2c896e50cf11624fb67970b3404ca
Radio <mraugust852020@outlook.com>
2026-02-24 11:05:57 -0500

    Update current.js

--- current.js
+++ current.js
@@ -1,6 +1,3 @@
 const link = "https://lxdo9udo.beanweb.qzz.io.cdn.cloudflare.net/";
-const aboutblank = window.open("","_blank");
 const htmldata = "<iframe src='"+link+"' style='border: 0; width: 100%; height: 100%'>Your browser does not support iFrames.</iframe>";
-if (aboutblank) {
-    aboutblank.document.write(htmldata);
-}
+document.write(htmldata);
