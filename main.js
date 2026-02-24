61b449690e038be3d58e51082e6ca5cf3fda723b
Radio <mraugust852020@outlook.com>
2026-02-24 15:22:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 const link = "https://lxdo9udo.beanweb.qzz.io.cdn.cloudflare.net/";
-document.getElementByTagName("iframe")[0].src = link;
+document.getElementById("iframe").setAttribute("src", link);
