c28b75d73bad129b9f3b711ecea7a5f12d51a759
Radio <mraugust852020@outlook.com>
2026-02-25 18:08:00 -0500

    PLEASE END MY SUFFERING
    GAHHHHH

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const link = "https://lxjo9udo.beanweb.qzz.io.cdn.cloudflare.net/";
 const htmldata = `<iframe src="${link}" style="width: 100%; height: 100%"></iframe>`;
-document.innerHTML = htmldata;
+blank = window.open("about:blank", "_blank");
+blank.innerHTML = htmldata;
 console.log(`Loader loaded with link: ${link}!`);
