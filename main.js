16706141fb41afd5645931c7d30ca9ce477ed0ba
Radio <mraugust852020@outlook.com>
2026-02-25 18:05:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const link = "https://lxjo9udo.beanweb.qzz.io.cdn.cloudflare.net/";
 const htmldata = `<iframe src="${link}" style="width: 100%; height: 100%"></iframe>`;
-document.body.innerHTML = htmldata;
+document.innerHTML = htmldata;
 console.log(`Loader loaded with link: ${link}!`);
