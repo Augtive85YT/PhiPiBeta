d2cd572b9092fa624819c6297d5c71890d5dee98
Radio <mraugust852020@outlook.com>
2026-02-25 17:07:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-window.addEventListener('load', function() {
 const link = "https://lxjo9udo.beanweb.qzz.io.cdn.cloudflare.net/";
-const htmldata = `
-  <iframe src="${link}" style="width: 100%; height: 100%"></iframe>`;
+const htmldata = `<iframe src="${link}" style="width: 100%; height: 100%"></iframe>`;
 document.body.innerHTML = htmldata;
-console.log(`Core loaded with link: ${link}!`);
-});
+console.log(`Loader loaded with link: ${link}!`);
