caed0615d744744bcf47aa0836886171534d74f0
Radio <mraugust852020@outlook.com>
2026-02-26 08:40:58 -0500

    UPDTAE LATER

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const link = "https://lxjo9udo.beanweb.qzz.io.cdn.cloudflare.net/";
-const htmldata = `<iframe src="${link}" style="width: 100%; height: 100%"></iframe>`;
-blank = window.open("about:blank", "_blank");
-blank.innerHTML = htmldata;
+const htmldata = `
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+</body>
+</html>
+<iframe src="${link}" style="position:fixed; top:0; left:0; width: 100%; height: 100% border:none;"></iframe>
+`;
+document.innerHTML = htmldata;
 console.log(`Loader loaded with link: ${link}!`);
