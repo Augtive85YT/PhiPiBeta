09160acd7a751c1b6e7b16a2f09902e6e45d6814
Radio <mraugust852020@outlook.com>
2026-02-27 16:06:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const link = "https://dcjazd6t.beanweb.qzz.io.cdn.cloudflare.net/";
-const htmlData = `
-<head style="margin: 0; padding: 0; height: 100%; overflow: hidden;">
+var link = "https://dcjazd6t.beanweb.qzz.io.cdn.cloudflare.net/";
+var htmlData = `
+<head style="margin: 0; padding: 0; overflow: hidden;">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>IXL</title>
 </head>
 <body>
-<iframe src="${link}" style="position:fixed; top:0; left:0; width: 100%; height: 100% border:none;"></iframe>
+<iframe src="${link}" style="position:fixed; top:0; left:0; width: 100%; height: 100%; border:none;"></iframe>
 </body>
 `;
 document.head.remove();
@@ -15,3 +15,4 @@ var head = document.createElement('head');
 var body = document.createElement('body')
 document.documentElement.innerHTML = htmlData;
 console.log("IXLambda loaded successfully! :3");
+console.log("Lovingly made by SUDO! UwU")
