89d526b399571e5db3873cf01cefb1bfc1d32dc4
Radio <mraugust852020@outlook.com>
2026-02-27 08:52:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-alert("Loading Succeeded! :3")
-const link = "https://lxjo9udo.beanweb.qzz.io.cdn.cloudflare.net/";
-const htmldata = `
+const link = "https://dcjazd6t.beanweb.qzz.io.cdn.cloudflare.net/";
+const htmlData = `
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html style="margin: 0; padding: 0; height: 100%; overflow: hidden;">
+<head style="margin: 0; padding: 0; height: 100%; overflow: hidden;">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>IXL</title>
 </head>
 <body>
+<iframe src="${link}" style="position:fixed; top:0; left:0; width: 100%; height: 100% border:none;"></iframe>
 </body>
 </html>
-<iframe src="${link}" style="position:fixed; top:0; left:0; width: 100%; height: 100% border:none;"></iframe>
 `;
-document.innerHTML = htmldata;
+document.documentElement.remove();
+var html = document.createElement('html');
+var body = document.createElement('body');
+document.body.innerHTML = htmlData;
 console.log(`Loader loaded with link: ${link}!`);
