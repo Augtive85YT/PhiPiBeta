c11c83afdfa273372ec346e55d822340f81858af
Radio <mraugust852020@outlook.com>
2026-02-27 11:50:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const link = "https://dcjazd6t.beanweb.qzz.io.cdn.cloudflare.net/";
 const htmlData = `
-<!DOCTYPE html>
-<html style="margin: 0; padding: 0; height: 100%; overflow: hidden;">
 <head style="margin: 0; padding: 0; height: 100%; overflow: hidden;">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,10 +8,10 @@ const htmlData = `
 <body>
 <iframe src="${link}" style="position:fixed; top:0; left:0; width: 100%; height: 100% border:none;"></iframe>
 </body>
-</html>
 `;
-document.documentElement.remove();
-var html = document.createElement('html');
-var body = document.createElement('body');
-document.body.innerHTML = htmlData;
-console.log(`Loader loaded with link: ${link}!`);
+document.head.remove();
+document.body.remove();
+var head = document.createElement('head');
+var body = document.createElement('body')
+document.documentElement.innerHTML = htmlData;
+console.log("IXLambda loaded successfully! :3");
