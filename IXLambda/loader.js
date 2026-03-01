00b9d71d89c28c21faf6807e2fb1cf15ef120285
Radio <mraugust852020@outlook.com>
2026-02-28 21:35:33 -0500

    Update loader.js

--- IXLambda/loader.js
+++ IXLambda/loader.js
@@ -1,3 +1,3 @@
 var script = document.createElement("script");
-script.src = "https://raw.githack.com/Augtive85YT/PhiPiBeta/refs/heads/main/IXLambda/main.js";
+script.src = "https://cdn.statically.io/gh/Augtive85YT/PhiPiBeta@main/IXLambda/main.js";
 document.head.appendChild(script);
