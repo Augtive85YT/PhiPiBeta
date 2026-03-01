2e755e828e67a65f39126bf3ee5ef6b971e4a1d6
Radio <mraugust852020@outlook.com>
2026-03-01 11:07:37 -0500

    Update loader.js

--- MSTaums/loader.js
+++ MSTaums/loader.js
@@ -1,3 +1,3 @@
 var script = document.createElement("script");
-script.src = "https://cdn.statically.io/gh/Augtive85YT/PhiPiBeta@main/MSTaums/main.js";
+script.src = "https://cdn.jsdelivr.net/gh/Augtive85YT/PhiPiBeta@main/MSTaums/main.js";
 document.head.appendChild(script);
