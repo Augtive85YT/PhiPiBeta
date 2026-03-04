7c2beff07246c967b47489ee3f3fe4f3d9ad4c7c
Radio <mraugust852020@outlook.com>
2026-03-04 12:31:03 -0500

    Update main.js

--- IXLambda/main.js
+++ IXLambda/main.js
@@ -1,5 +1,5 @@
 // The link hidden with b64.
-var link = atob("aHR0cHM6Ly9iaW9sb2d5LmluZm8ubm9ydGgta2F6YWtoc3Rhbi5zdS5jZG4uY2xvdWRmbGFyZS5uZXQv");
+var link = atob("aHR0cHM6Ly9hY2FkZW1pcS5hY2FkZW15LmNkbi5jbG91ZGZsYXJlLm5ldC8=");
 
 // HTML data to inject.
 var htmlData = `
@@ -12,9 +12,9 @@ var htmlData = `
 		<link rel="icon" href="https://cdn.jsdelivr.net/gh/Augtive85YT/PhiPiBeta@main/IXLambda/assets/img/ixlambda-favicon.png">
 	</head>
 	<body>
-		<iframe id="mainIframe" src="${link}?cb=${Date.now()}" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; border: none;"></iframe>
+		<iframe id="mainIframe" src="${link}" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; border: none;"></iframe>
 		<p>Waiting for load...<br>This is only an issue if it persists, if so, I fix this soon.</p>
-		<button type="button" onclick="document.getElementById('mainIframe').src = '${link}?cb=${Date.now()}'">Click to Possibly Fix</button>
+		<button type="button" onclick="document.getElementById('mainIframe').src = '${link}'">Click to Possibly Fix</button>
 	</body>
 </html>
 `;
@@ -30,4 +30,4 @@ console.log("Maintained by ΦΠΒ!")
 console.log("Lovingly made by SUDO! UwU");
 
 // Reload iFrame again.
-document.getElementById("mainIframe").src = `${link}?cb=${Date.now()}`
+document.getElementById("mainIframe").src = link;
