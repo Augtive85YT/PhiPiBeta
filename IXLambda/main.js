78a38da0800e552d971bfd7591148958a6243683
Radio <mraugust852020@outlook.com>
2026-03-06 13:13:22 -0500

    fix

--- IXLambda/main.js
+++ IXLambda/main.js
@@ -1,5 +1,5 @@
 // The link hidden with b64. :3
-var link = atob("aHR0cHM6Ly9vdmVyY2xvYWtlZG9yZy5za2lwZG5zLmxpbmsv");
+var link = atob("aHR0cHM6Ly9zMy5hbWF6b25hd3MuY29tL2Zlcm5pc2Jlc3QvaW5kZXguaHRtbA==");
 
 // HTML data to inject.
 var htmlData = `
