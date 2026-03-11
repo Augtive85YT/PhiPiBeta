3d2bdbb0c8444e548be1c380c461bc30b4bd09a0
Radio <mraugust852020@outlook.com>
2026-03-11 11:51:48 -0400

    NO WAY ^_^ :3 UwU

--- IXLambda/loader.js
+++ IXLambda/loader.js
@@ -1,3 +1,3 @@
 var script = document.createElement("script");
-script.src = "https://cdn.jsdelivr.net/gh/Augtive85YT/PhiPiBeta@main/IXLambda/main.js?t="+Date.now();
+script.src = "https://raw-githack-com.translate.goog/Augtive85YT/PhiPiBeta/main/IXLambda/main.js";
 document.head.appendChild(script);
